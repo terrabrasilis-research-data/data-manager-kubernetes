924f5cb5b4c9a079b286c7aa72b8513d005f8c80
GSansigolo <gabrielsansigolo@gmail.com>
2020-05-11 16:48:11 -0300

    Finish create, read and delete

--- kubernetes-api/app.py
+++ kubernetes-api/app.py
@@ -8,9 +8,11 @@ from flask_cors import CORS
 from yaml import load, dump
 from flask import request
 from flask import url_for
+from flask import abort
 import subprocess
 import yaml
 import os 
+import re
 
 # env
 def get_env_variable(name):
@@ -36,9 +38,24 @@ def not_found(error):
 #+--------------------------------------------------------+
 #| read()                                                 |
 #+--------------------------------------------------------+
-@app.route('/api/v1.0/read', methods=['GET'])
-def read_pods():
-        
+@app.route('/api/v1.0/read', methods=['PUT'])
+def read_pods():    
+            
+    #  {
+    #      "namespace": "gabriel",
+    #  }
+
+    if not request.json or not 'namespace' in request.json:
+        abort(400)
+    
+    namespace=request.json['namespace']
+            
+    pattern = '[a-z0-9]([-a-z0-9]*[a-z0-9])'
+    result = re.match(pattern, namespace)
+
+    if not result:
+        abort(400)
+
     # Configs can be set in Configuration class directly or using helper utility
     config.load_kube_config()
     
@@ -46,7 +63,7 @@ def read_pods():
     v1 = client.CoreV1Api()
     
     # list pods
-    pod_list = v1.list_namespaced_pod("default")
+    pod_list = v1.list_namespaced_pod(namespace)
     pod_list_json = []
     
     # read pods
@@ -61,13 +78,31 @@ def read_pods():
 #+--------------------------------------------------------+
 #| create()                                               |
 #+--------------------------------------------------------+
-@app.route('/api/v1.0/create', methods=['GET'])
+@app.route('/api/v1.0/create', methods=['PUT'])
 def create_repository():
+    
+    #  {
+    #      "namespace": "gabriel",
+    #      "services": ["geoserver", "nginx", "postgis", "volume"]
+    #  }
 
-    yaml_folders = ['geonetwork', 'geoserver', 'nginx', 'owncloud', 'postgis', 'terrama2', 'volume']
-   
+    if not request.json or not 'namespace' and 'services' in request.json:
+        abort(400)
+
+    namespace=request.json['namespace']
+    services=request.json['services']
+    
+    pattern = '[a-z0-9]([-a-z0-9]*[a-z0-9])'
+    result = re.match(pattern, namespace)
+
+    if not result:
+        abort(400)
+
+    # create a namespace
+    subprocess.call('kubectl create namespace ' + str(namespace), shell=True)
+
     # open folders
-    for folder in yaml_folders:
+    for folder in services:
         
         # list the yamls
         yamls_list = os.listdir(os.path.join(STATIC_FOLDER, folder))
@@ -83,7 +118,9 @@ def create_repository():
                 dep = yaml.safe_load(file)
 
             # edit
-    
+            dep['metadata']['name'] = dep['metadata']['name'] + '-' + str(namespace)
+            dep['metadata']['namespace'] = str(namespace)
+
             # save yaml
             with open(os.path.join(STATIC_FOLDER, 'edited_file.yaml'), 'w') as outfile:
                 yaml.dump(dep, outfile, default_flow_style=False)
@@ -97,37 +134,26 @@ def create_repository():
 #+--------------------------------------------------------+
 #| delete()                                               |
 #+--------------------------------------------------------+
-@app.route('/api/v1.0/delete', methods=['GET'])
+@app.route('/api/v1.0/delete', methods=['PUT'])
 def delete_repository():
         
-    # Configs can be set in Configuration class directly or using helper utility
-    config.load_kube_config()
-    
-    # kubernetes api
-    v1 = client.CoreV1Api()
-    
-    # list pods
-    pod_list = v1.list_namespaced_pod("default")
-    pod_list_json = []
-    
-    # read pods
-    for pod in pod_list.items:
-        pod_list_json.append({"service_name": pod.metadata.name.split('-')[0]})
+    #  {
+    #      "namespace": "gabriel",
+    #  }
 
-    # delete configmap
-    subprocess.call('kubectl delete configmap nginx-conf', shell=True)
+    if not request.json or not 'namespace' in request.json:
+        abort(400)
     
-    # delete ingress
-    subprocess.call('kubectl delete ingress ingress-nginx', shell=True)
+    namespace=request.json['namespace']
+            
+    pattern = '[a-z0-9]([-a-z0-9]*[a-z0-9])'
+    result = re.match(pattern, namespace)
 
-    # delete pods and services
-    for item in pod_list_json:
+    if not result:
+        abort(400)
 
-        # delete service
-        subprocess.call('kubectl delete service ' + item['service_name'], shell=True)
-        
-        # delete deployment
-        subprocess.call('kubectl delete deployment ' + item['service_name'], shell=True)
+    # delete namespace
+    subprocess.call('kubectl delete namespace ' + str(namespace), shell=True)
 
     # return
     return jsonify({"result":"sucess"})
